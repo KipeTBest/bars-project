5338181d76c1e942cdb1e9eaf2cfc44f8e3f44fc
Ramil <miftakhutdinov.ramil@bk.ru>
2024-06-25 00:38:57 +0300

    I made a login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,5 +1,28 @@
-const Login = ()=>{
-
+import "./Login.css"
+import { useState } from 'react'
+import LoginButton from "./LoginButton/LoginButton"
+import { Link } from "react-router-dom";
+const Login = () => {
+    
+    return(
+        <div className="login">
+            <div className='login-form'>
+                <h1 className="login-title">Войдите в систему</h1>
+                <div className="input-info">
+                    <span className="text-input">Почта</span>
+                    <input type='title' name='login' className="log"/>
+                </div>
+                <div className="input-info">
+                    <span className="text-input">Пороль</span>
+                    <input type='password' name='pass' className="pass"/>
+                </div>
+                <div className="login-buttons">
+                    <Link to=""><LoginButton style={50} data={"Вход"} /></Link>
+                    <Link to=""><LoginButton style={50} data={"Регистация"}/></Link>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Login
