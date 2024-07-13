d66a8961fea952e95dffabae86dad19b5eadbb66
KatrinBnk <yekaterinabanko@gmail.com>
2024-07-13 19:53:20 +0300

    Update LoadJson.js

--- src/Components/DBComponents/LoadJson.js
+++ src/Components/DBComponents/LoadJson.js
@@ -7,8 +7,10 @@ const LoadJson = () => {
                 // Проверяем, есть ли данные в localStorage, чтобы не заливать их повторно
                 const existingData = localStorage.getItem('data');
                 const existingUser = localStorage.getItem('user');
+                const existingUsers = localStorage.getItem('users');
+                const existingReviews = localStorage.getItem('reviews');
 
-                if (!existingData || !existingUser) {
+                if (!existingData || !existingUser || !existingUsers || !existingReviews) {
                     const responseData = await fetch('/movies.json');
                     const data = await responseData.json();
                     localStorage.setItem('data', JSON.stringify(data));
@@ -17,13 +19,18 @@ const LoadJson = () => {
                     const user = await responseUser.json();
                     localStorage.setItem('user', JSON.stringify(user));
 
+                    const responseUsers = await fetch('/users.json');
+                    const users = await responseUsers.json();
+                    localStorage.setItem('users', JSON.stringify(users));
+
                     const responseReviews = await fetch('/reviews.json');
-                    const dataReviews = await responseReviews.json();
-                    localStorage.setItem('reviews', JSON.stringify(dataReviews));
+                    const reviews = await responseReviews.json();
+                    localStorage.setItem('reviews', JSON.stringify(reviews));
 
                     console.log('Данные фильмов загружены:', data);
                     console.log('Данные пользователя загружены:', user);
-                    console.log('Данные отзывов загружены:', dataReviews);
+                    console.log('Данные отзывов загружены:', reviews);
+                    console.log('Данные о пользователях загружены:', users);
                 } else {
                     console.log('Данные уже загружены в localStorage.');
                 }
